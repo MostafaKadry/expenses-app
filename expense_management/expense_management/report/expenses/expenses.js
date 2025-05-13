a0f60fe38039638c0a76f16bb13e07b6302cddb1
MostafaKadry <mostafakadry806@gmail.com>
2025-05-13 09:24:14 +0300

    remove unnccessary comments

--- expense_management/expense_management/report/expenses/expenses.js
+++ expense_management/expense_management/report/expenses/expenses.js
@@ -1,24 +1,6 @@
 frappe.query_reports["Expenses by Category"] = {
 
     
-    // onload: function(report) {
-    //     report.page.add_action("Export", function() {
-    //         frappe.call({
-    //             method: "frappe.desk.query_report.export_to_csv",
-    //             args: {
-    //                 report_name: "Expenses by Category",
-    //                 filters: report.filters
-    //             },
-    //             callback: function(r) {
-    //                 if (r.message) {
-    //                     var url = r.message;
-    //                     window.location.href = url;  
-    //                 }
-    //             }
-    //         });
-    //     });
-    // },
-
     // Refresh the report after applying filters
     refresh: function(report) {
         const filters = report.filters;
